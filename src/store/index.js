0d2a08a4be34cceab60d4140d61c93d4b307f4ac
nninnnin <nninnnin7@gmail.com>
2022-04-30 17:44:10 +0900

    Add redux logger middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
+import logger from "redux-logger";
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "../reducers";
 
 const store = configureStore({
   reducer: rootReducer,
+  middleware: [logger],
 });
 
 export default store;
