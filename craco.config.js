771806a71368b7fb64698b5e78015985b09d92d5
nninnnin <nninnnin7@gmail.com>
2022-04-30 17:42:55 +0900

    Update path alias settings for ts files

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,17 @@
-const path = require("path");
+const CracoAlias = require("craco-alias");
 
 module.exports = {
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        baseUrl: ".",
+        tsConfigPath: "./tsconfig.extend.json",
+      },
+    },
+  ],
   webpack: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-      "@components": path.resolve(__dirname, "src/components"),
-    }
-  }
+    extensions: [".ts", ".tsx", ".js"],
+  },
 };
